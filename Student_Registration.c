24443f4ecf0db5c0554049e7f4560de08da1689d
vaishnavi202134 <147791955+vaishnavi202134@users.noreply.github.com>
2023-11-23 10:50:56 +0530

    #include<stdio.h> int main() {     int t,n,m,k;     scanf("%d",&t);     while(t--)     {         scanf("%d%d%d",&n,&m,&k);        if(m-k>=n) printf("YES ");        else printf("NO ");     } }

---
+++ Student_Registration.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int t,n,m,k;
+    scanf("%d",&t);
+    while(t--)
+    {
+        scanf("%d%d%d",&n,&m,&k);
+       if(m-k>=n) printf("YES
+");
+       else printf("NO
+");
+    }
+}
