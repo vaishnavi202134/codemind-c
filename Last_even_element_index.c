c1826201aa664296fd424d25b26e4a4906da26f6
vaishnavi202134 <147791955+vaishnavi202134@users.noreply.github.com>
2024-01-24 20:38:51 +0530

    #include<stdio.h> int main() {     int a,i,arr[a],b;     scanf("%d",&a);     for(i=0;i<a;i++)     {         scanf("%d",&arr[i]);         if(arr[i]%2==0) b=i;     }     printf("%d",b); }

---
+++ Last_even_element_index.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,arr[a],b;
+    scanf("%d",&a);
+    for(i=0;i<a;i++)
+    {
+        scanf("%d",&arr[i]);
+        if(arr[i]%2==0) b=i;
+    }
+    printf("%d",b);
+}
